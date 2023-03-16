54aeb5993fa251d0b68db106e0e6f3a37d07c01e
Rachid Amhouch <ramhouch@e2r10p11.1337.ma>
2023-03-16 12:39:10 +0100

    Push

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * main - puts
+ * Return: 1 if the program finish whitout errors
+ */
+int	main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (0);
+}
