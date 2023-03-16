432a1e97349f074ba473d873527d38272c6c523f
Rachid Amhouch <ramhouch@e2r10p11.1337.ma>
2023-03-16 12:20:36 +0100

    Push

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,12 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+/**
+ * main - puts
+ */
+
 int	main(void)
 {
-	/* main function */
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
