983c414a93d775f10126d29cf352fe8a0b33b4d2
Rachid Amhouch <ramhouch@e2r10p11.1337.ma>
2023-03-16 12:27:37 +0100

    Push

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * main - puts
+ * Return: 0 if the program finish whitout errors
+ */
+int	main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
