700e17b4e1a5c088752ef7282f69cddabcd129e6
Rachid Amhouch <ramhouch@e2r10p9.1337.ma>
2023-03-17 14:12:22 +0100

    Push

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * main - main function
+ * Return: 0 if the program finish whitout errors
+ */
+int	main(void)
+{
+	char	n;
+
+	n = 'a';
+	while (n <= 'z')
+	{
+		putchar(n);
+		n++;
+	}
+	n = 'A';
+	while (n <= 'Z')
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
