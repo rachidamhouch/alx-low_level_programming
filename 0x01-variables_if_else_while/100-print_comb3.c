b05279adbf85223d0856a62a9fbbc87f43e5a27d
Rachid Amhouch <ramhouch@e2r10p9.1337.ma>
2023-03-17 15:54:40 +0100

    Push

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,22 +9,25 @@
 int	main(void)
 {
 	int	i;
-	int	j
+	int	j;
 
 	i = 0;
 	while (i < 10)
 	{
-		j += i + 1;
+		j = i + 1;
 		while(j < 10)
 		{
 			putchar('0' + i);
 			putchar('0' + j);
-			if (i != 8 && j != 9)
+			if (i != 8 || j != 9)
 			{
 				putchar(',');
 				putchar(' ');
 			}
+			j++;
 		}
+		i++;
 	}
+	putchar('\n');
 	return (0);
 }
