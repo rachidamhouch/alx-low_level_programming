aacd35d97a0258a0adc43b945d5e5c6676c61cd8
Rachid Amhouch <ramhouch@e2r10p9.1337.ma>
2023-03-17 14:36:26 +0100

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * main - main function
+ * Return: 0 if the program finish whitout errors
+ */
+int	main(void)
+{
+	char	n;
+
+	n = '0';
+	while (n <= '9')
+	{
+		putchar(n);
+		n++;
+	}
+	n = 'a';
+	while (a <= 'f')
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
