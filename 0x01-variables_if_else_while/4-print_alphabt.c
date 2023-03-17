2eea2f15f0102d0b3c4c018dc761498666f14848
Rachid Amhouch <ramhouch@e2r10p9.1337.ma>
2023-03-17 14:15:51 +0100

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * main - main function
+ * Return: 0 if the program finish whitout errors
+ */
+int	main(void)
+{
+	char	n;
+
+	n = 'a';
+	while (n <= 'z')
+	{
+		if (n != 'q' && n != 'e')
+			putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
