c68c65451b6f6012f6d444387dd190ed2a78f9ee
Rachid Amhouch <ramhouch@e2r10p9.1337.ma>
2023-03-17 15:50:44 +0100

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * main - main function
+ * Return: 0 if the program finish whitout errors
+ */
+int	main(void)
+{
+	int	i;
+	int	j
+
+	i = 0;
+	while (i < 10)
+	{
+		j += i + 1;
+		while(j < 10)
+		{
+			putchar('0' + i);
+			putchar('0' + j);
+			if (i != 8 && j != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	return (0);
+}
