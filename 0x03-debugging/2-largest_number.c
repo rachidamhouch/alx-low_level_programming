4bdd6e594af454a2292d589b798b8f17c7be7b91
Rachid Amhouch <ramhouch@e2r9p3.1337.ma>
2023-03-21 22:12:43 +0000

    Push

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,3 +1,5 @@
+
+
 #include "main.h"
 
 /**
@@ -8,21 +10,22 @@
  * Return: largest number
  */
 
-int	largest_number(int a, int b, int c)
+int largest_number(int a, int b, int c)
 {
-    int	largest;
+	int largest;
 
-    if (a > b && a > c)
-    {
-        largest = a;
-    }
-    else if (b > a && b > c)
-    {
-        largest = b;
-    }
-    else
-    {
-        largest = c;
-    }
-    return (largest);
+	if (a >= b && a >= c)
+	{
+		largest = a;
+	}
+	else if (b >= a && b >= c)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
+
+	return (largest);
 }
