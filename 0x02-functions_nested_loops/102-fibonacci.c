36f0e4db66371df2725644ca7d0fe00e65c92e0f
Rachid Amhouch <ramhouch@e2r8p9.1337.ma>
2023-03-21 17:51:36 +0000

    Push

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * fibonacci - fibonacci
+ * Return: 0 if the program finish whitout errors
+ */
+int	fibonacci(int index)
+{
+	if (index < 0)
+		return (-1);
+	if (index == 0)
+		return (0);
+	if (index == 1)
+		return (1);
+	return (fibonacci(index - 1) + fibonacci(index - 2));
+}
+
+/**
+ * main - main function
+ * Return: 0 if the program finish whitout errors
+ */
+int	main(void)
+{
+	printf("%d\n", fibonacci(3));
+	return (0);
+}
