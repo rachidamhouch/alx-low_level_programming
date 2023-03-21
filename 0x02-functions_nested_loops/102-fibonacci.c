04f490f943428e34de579966293c34f84582e540
Rachid Amhouch <ramhouch@e2r8p9.1337.ma>
2023-03-21 18:09:50 +0000

    Push

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,28 +1,27 @@
 #include "main.h"
 /**
- * fibonacci - fibonacci
- * Return: 0 if the program finish whitout errors
- * @index: First arg.
- */
-long	fibonacci(int index)
-{
-	if (index < 0)
-		return (-1);
-	if (index == 0)
-		return (1);
-	if (index == 1)
-		return (2);
-	return (fibonacci(index - 1) + fibonacci(index - 2));
-}
-
-/**
  * main - main function
  * Return: 0 if the program finish whitout errors
  */
 int	main(void)
 {
-	int	i;
-	while (i < 50)
-		printf("%ld\n", fibonacci(i++));
-	return (0);
+	int				i;
+    long long int	fib1;
+	long long int	fib2;
+	long long int	next;
+
+	fib1 = 1;
+	fib2 = 2;
+    printf("%lld, %lld ", fib1, fib2);
+    for(i=3; i<=50; i++)
+	{
+        next = fib1 + fib2;
+        printf("%lld", next);
+		if (i != 50)
+			printf(", ");
+        fib1 = fib2;
+        fib2 = next;
+    }
+    printf("\n");
+    return 0;
 }
