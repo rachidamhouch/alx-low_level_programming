dc72fdbea177f4bfc165e0a2963d0c36bef2d515
Rachid Amhouch <ramhouch@e2r8p9.1337.ma>
2023-03-21 14:56:08 +0000

    Push

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * alx_putnbr - prints numbers < 1000
+ * @n: first parameter
+ */
+void	alx_putnbr(int n)
+{
+	if (n < 10)
+	{
+		putchar('0');
+		putchar('0' + n);
+	}
+	else
+	{
+		putchar('0' + n / 10);
+		putchar('0' + n % 10);
+	}
+}
+
+/**
+ * jack_bauer - function that prints every minute of the day.
+ */
+void jack_bauer()
+{
+	int		h;
+	int		m;
+
+	h = 0;
+	m = 0;
+	while (h < 24)
+	{
+		alx_putnbr(h);
+		write(1, ":", 1);
+		alx_putnbr(m);
+		write(1, "\n", 1);
+		m++;
+		if (m == 60)
+			h++;
+	}
+}
