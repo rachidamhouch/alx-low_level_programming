8a0634b82c320d730f5c73178892a8607b20b6d0
Rachid Amhouch <ramhouch@e2r8p9.1337.ma>
2023-03-21 13:16:20 +0000

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * main - main function
+ * Return: 0 if the program finish whitout errors
+ */
+int	main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
