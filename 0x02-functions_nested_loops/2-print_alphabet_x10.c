a13b15b7675c4cc14710c1117ec2a2567b439a6a
Rachid Amhouch <ramhouch@e2r8p9.1337.ma>
2023-03-21 13:35:36 +0000

    Push

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,5 +1,18 @@
 #include "main.h"
 /**
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new line.
+ */
+void	print_alphabet(void)
+{
+	char	a;
+
+	a = 'a';
+	while (a <= 'z')
+		putchar(a++);
+	putchar('\n');
+}
+
+/**
  * print_alphabet_x10 -  prints 10 times the alphabet, in lowercase, followed by a new line.
  */
 void	print_alphabet_x10(void)
