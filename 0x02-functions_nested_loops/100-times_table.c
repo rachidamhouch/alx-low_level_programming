bc27342f39d9497457690c5f5c00ebed2cf382f1
Rachid Amhouch <ramhouch@e2r8p9.1337.ma>
2023-03-21 16:49:32 +0000

    Push

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,50 @@
+#include "main.h"
+/**
+ * alx_putnbr - prints numbers < 1000
+ * @n: first arg'
+ * @m: second arg.
+ */
+void	alx_putnbr(int n, int m)
+{
+	if (n * m < 10)
+	{
+		if (m)
+			putchar(' ');
+		putchar('0' + n * m);
+	}
+	else
+	{
+		putchar('0' + (n * m) / 10);
+		putchar('0' + (n * m) % 10);
+	}
+}
+
+/**
+ * print_times_table - function that prints the n times table, starting with 0.
+ */
+void print_times_table(int n)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	if (n >= 0 && n <= 15)
+	{
+		while (i < n)
+		{
+			j = 0;
+			while (j < n)
+			{
+				alx_putnbr(i, j);
+				if (j != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				j++;
+			}
+			putchar('\n');
+			i++;
+		}
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -16,4 +16,5 @@ void 	jack_bauer();
 void 	times_table(void);
 int     add(int n, int m);
 void    print_to_98(int n);
+void    print_times_table(int n);
 #endif
