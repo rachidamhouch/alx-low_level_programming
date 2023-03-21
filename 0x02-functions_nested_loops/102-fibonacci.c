5c1a4a299214f0c33336af195428d915318b6402
Rachid Amhouch <ramhouch@e2r8p9.1337.ma>
2023-03-21 18:12:11 +0000

    Push

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,22 +6,21 @@
 int	main(void)
 {
 	int			i;
-    long int	fib1;
+	long int	fib1;
 	long int	fib2;
-	long int	next;
-
+	long int	next;	
 	fib1 = 1;
 	fib2 = 2;
-    printf("%ld, %ld ", fib1, fib2);
-    for(i=3; i<=50; i++)
+	printf("%ld, %ld ", fib1, fib2);
+	for (i = 3; i <= 50; i++)
 	{
-        next = fib1 + fib2;
-        printf("%ld", next);
+	    next = fib1 + fib2;
+	    printf("%ld ", next);
 		if (i != 50)
-			printf(", ");
-        fib1 = fib2;
-        fib2 = next;
-    }
-    printf("\n");
-    return 0;
+			printf(",");
+	    fib1 = fib2;
+	    fib2 = next;
+	}
+	printf("\n");
+	return 0;
 }
