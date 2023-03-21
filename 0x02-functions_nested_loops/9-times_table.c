7f1df1fde18144f2f9e4a5ba8f6d67c3b99e59f1
Rachid Amhouch <ramhouch@e2r8p9.1337.ma>
2023-03-21 15:08:34 +0000

    Push

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * alx_putnbr - prints numbers < 1000
+ * @n: first parameter
+ */
+void	alx_putnbr(int n)
+{
+	if (n < 10)
+		putchar('0' + n);
+	else
+	{
+		putchar('0' + n / 10);
+		putchar('0' + n % 10);
+	}
+}
+
+/**
+ * times_table - function that prints the 9 times table, starting with 0.
+ */
+void times_table(void)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (i < 10)
+	{
+		while (j < 10)
+		{
+			alx_putnbr(i * j);
+			putchar(' ');
+			putchar(' ');
+			putchar(',');
+		}
+		putchar('\n');
+	}
+}
