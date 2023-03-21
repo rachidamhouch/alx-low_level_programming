273b0a1556e5402f3fcb6250264ad05d53b50389
Rachid Amhouch <ramhouch@e2r8p9.1337.ma>
2023-03-21 18:26:59 +0000

    Push

---
+++ 0x02-functions_nested_loops/102-fibonacci copy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * main - main function
+ * Return: 0 if the program finish whitout errors
+ */
+int	main(void)
+{
+	int			i;
+	long int	fib1;
+	long int	fib2;
+	long int	next;
+
+	fib1 = 1;
+	fib2 = 2;
+	i = 2;
+	printf("%ld, %ld, ", fib1, fib2);
+	while (i < 98)
+	{
+		next = fib1 + fib2;
+		printf("%ld", next);
+		if (i != 49)
+			printf(", ");
+		fib1 = fib2;
+		fib2 = next;
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
