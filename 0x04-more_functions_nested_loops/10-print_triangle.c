85043fff5d5a4a7eb0fd5cc681c297a5e3c3dbba
Rachid Amhouch <ramhouch@e2r11p10.1337.ma>
2023-03-23 21:45:24 +0000

    Push

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -33,3 +33,11 @@ void	print_triangle(int size)
 		i++;
 	}
 }
+int main(void)
+{
+    print_triangle(2);
+    print_triangle(10);
+    print_triangle(1);
+    print_triangle(0);
+    return (0);
+}
