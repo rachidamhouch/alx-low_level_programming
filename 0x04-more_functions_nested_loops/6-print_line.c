f6847a98be8b53cd4daddc6173fe0eeb30495760
Rachid Amhouch <ramhouch@e2r10p8.1337.ma>
2023-03-23 18:37:09 +0000

    Push

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+# include "main.h"
+
+/**
+ * print_line - function that draws a straight line in the terminal.
+ * @n: arg 1;
+ */
+void	print_line(int n)
+{
+	int	i;
+
+	i = 0;
+	while (i < n)
+	{
+		_putchar('_');
+		i++;
+	}
+	_putchar('\n');
+}
