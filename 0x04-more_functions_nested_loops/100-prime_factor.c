c0e86f79ae369545cd958047919599e02208cedb
Rachid Amhouch <ramhouch@e2r11p10.1337.ma>
2023-03-23 23:02:55 +0000

    Push

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * is_prime - is prime.
+ * @nb: arg 1.
+ * Return: 1 if is prime.
+ */
+int	is_prime(long int nb)
+{
+	int	i;
+
+	i = 2;
+	if (nb <= 1)
+		return (0);
+	if (nb == 2)
+		return (1);
+	while (i <= sqrt(nb))
+	{
+		if (nb % i == 0)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+/**
+ * main - finds and prints the largest prime factor of the number 612852475143
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	long int n;
+	long int max;
+	long int i;
+
+	n = 1231952;
+	while (i <= n)
+	{
+		while (is_prime(i) && n % i == 0)
+		{
+			max = i;
+			n /= i;
+		}
+		i++;
+	}
+	printf("%ld\n", max);
+	return (0);
+}
