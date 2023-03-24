18606e9147eceb3e3cc925a54ee848cbde7c19a6
Rachid Amhouch <ramhouch@e2r11p10.1337.ma>
2023-03-24 00:48:35 +0000

    Push

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,10 +1,29 @@
 #include "main.h"
 /**
+ * printstr - function that prints string.
+ * @str: arg 1.
+ */
+void printstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
+
+/**
  * print_number - prints numbers
  * @n: first parameter
  */
 void	print_number(int n)
 {
+	if (n == INT_MIN)
+		printstr("-2147483648");
 	if (n < 0)
 	{
 		_putchar('-');
