3734d51aa79066292e69e2215b1f292617ddc4ff
Rachid Amhouch <ramhouch@e2r8p5.1337.ma>
2023-03-27 20:30:08 +0000

    Push

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   0-reset_to_98.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ramhouch <ramhouch@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/27 20:26:48 by ramhouch          #+#    #+#             */
+/*   Updated: 2023/03/27 20:29:44 by ramhouch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "main.h"
+
+/**
+ * reset_to_98 - function that takes a pointer to an int as
+ * parameter and updates the value it points to to 98.
+ * n: arg 1.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
