e641f5d48c26075562841824dd987dbf29c14123
Rachid Amhouch <ramhouch@e2r8p5.1337.ma>
2023-03-27 22:23:54 +0000

    Push

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   100-atoi.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ramhouch <ramhouch@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/27 22:11:37 by ramhouch          #+#    #+#             */
+/*   Updated: 2023/03/27 22:23:23 by ramhouch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "main.h"
+
+/**
+ * _atoi - function that convert a string to an integer.
+ * @s: arg 1.
+ * Return: an integer.
+ */
+int _atoi(char *s)
+{
+	unsigned int	n;
+	int				sign;
+	int				i;
+
+	n = 0;
+	i = 0;
+	sign = 0;
+	while (s[i] && s[i] == ' ' && s[i] == '\t')
+		i++;
+	while (s[i] && s[i] == '+' && s[i] == '-')
+	{
+		if (s[i] == '-')
+			sign++;
+		i++;
+	}
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		n *= 10;
+		n += s[i] - '0';
+		i++;
+	}
+	if (sign % 2 == 0)
+		return (n);
+	return (-n);
+}
