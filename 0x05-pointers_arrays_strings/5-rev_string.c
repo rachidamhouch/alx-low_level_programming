e578c78613d4d6ff28a6aef9f6a8271d7384403d
Rachid Amhouch <ramhouch@e2r8p5.1337.ma>
2023-03-27 21:00:24 +0000

    Push

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   5-rev_string.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ramhouch <ramhouch@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/27 20:56:37 by ramhouch          #+#    #+#             */
+/*   Updated: 2023/03/27 21:00:20 by ramhouch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "main.h"
+
+/**
+ * _strlen - function that returns the length of a string.
+ * @s: arg 1.
+ * Return: the length of a string.
+ */
+int _strlen(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+/**
+ * swap_int - function that swaps the values of two integers.
+ * @a: arg 1.
+ * @b: arg 2.
+ */
+void swap_int(char *a, char *b)
+{
+	char	i;
+
+	i = *a;
+	*a = *b;
+	*b = i;
+}
+
+/**
+ * rev_string - function that reverses a string.
+ * @s: arg 1.
+ */
+void rev_string(char *s)
+{
+	int	i;
+	int	j;
+
+	i = _strlen(s) - 1;
+	j = 0;
+	while (j < _strlen(s) / 2)
+	{
+		swap_int(s[j], s[i]);
+		j++;
+		i--;
+	}
+}
