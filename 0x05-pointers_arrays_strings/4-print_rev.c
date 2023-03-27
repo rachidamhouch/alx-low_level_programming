0a2768c10abe42177e438c6bf2ebf8a9be6ace60
Rachid Amhouch <ramhouch@e2r8p5.1337.ma>
2023-03-27 20:54:19 +0000

    Push

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,43 @@
+/* ******************************************* */
+/*                                             */
+/*                                             */
+/*   4-print_rev.c                             */
+/*                                             */
+/*   By: ramhouch <ramhouch@student.42.fr>     */
+/*                                             */
+/*   Created: 2023/03/27 20:51:27 by ramhouch  */
+/*   Updated: 2023/03/27 20:53:58 by ramhouch  */
+/*                                             */
+/* ******************************************* */
+
+# include "main.h"
+
+/**
+ * _strlen - function that returns the length of a string.
+ * @s: arg 1.
+ * Return: the length of a string.
+ */
+int _strlen(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+/**
+ * print_rev - function that prints a string, in reverse, followed by a new line.
+ * @s: arg 1.
+ */
+void print_rev(char *s)
+{
+	int	i;
+
+	i = _strlen(s);
+
+	while (i--)
+		write(1, s + i, 1);
+	write(1, "\n", 1);
+}
