e2afd6ee1b3b1fb6fa62ecc2241a83f9b078ced0
Rachid Amhouch <ramhouch@e2r8p5.1337.ma>
2023-03-27 21:54:15 +0000

    Push

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,45 @@
+/* ******************************************* */
+/*                                             */
+/*                                             */
+/*   7-puts_half.c                             */
+/*                                             */
+/*   By: ramhouch <ramhouch@student.42.fr>     */
+/*                                             */
+/*   Created: 2023/03/27 21:52:36 by ramhouch  */
+/*   Updated: 2023/03/27 21:53:54 by ramhouch  */
+/*                                             */
+/* ******************************************* */
+
+# include "main.h"
+
+/**
+ * _strlen - function that returns the length of a string.
+ * @s: arg 1.
+ * Return: the length of a string.
+ */
+int _strlen(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+/**
+ * puts_half - function that prints half of a string.
+ * @str: arg 1.
+ */
+void	puts_half(char *str)
+{
+	int	i;
+
+	i = _strlen(str) / 2;
+	while (str[i])
+	{
+		write(1, str + i, 1);
+		i++;
+	}
+	write(1, "\n", 1);
+}
