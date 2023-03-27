977d4c762f1036b7141697ffdc1f2f475f6b9c86
Rachid Amhouch <ramhouch@e2r8p5.1337.ma>
2023-03-27 20:40:17 +0000

    Push

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,27 @@
+/* ******************************************* */
+/*                                             */
+/*                                             */
+/*   2-strlen.c                                */
+/*                                             */
+/*   By: ramhouch <ramhouch@student.42.fr>     */
+/*                                             */
+/*   Created: 2023/03/27 20:38:34 by ramhouch  */
+/*   Updated: 2023/03/27 20:40:05 by ramhouch  */
+/*                                             */
+/* ******************************************* */
+
+# include "main.h"
+
+/**
+ * _strlen - function that returns the length of a string.
+ * @s: arg 1.
+ */
+int _strlen(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (0);
+}
