a23bed52e4c3b54fba8f4eb264e05371d451c649
Rachid Amhouch <ramhouch@e2r8p5.1337.ma>
2023-03-28 00:19:25 +0000

    Push

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   101-keygen.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ramhouch <ramhouch@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/28 00:13:30 by ramhouch          #+#    #+#             */
+/*   Updated: 2023/03/28 00:19:17 by ramhouch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "main.h"
+
+/**
+ * main - finds and prints the largest prime factor of the number 612852475143
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("HccccccNNN545@.\\n");
+	return (0);
+}
