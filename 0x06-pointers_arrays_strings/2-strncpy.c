6f20bc98e597703253d17d86e95966be9a4df65d
Rachid Amhouch <ramhouch@e2r9p5.1337.ma>
2023-03-29 20:41:54 +0000

    Push

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,34 @@
+/* ******************************************* */
+/*                                             */
+/*                                             */
+/*   2-strncpy.c                               */
+/*                                             */
+/*   By: ramhouch <ramhouch@student.42.fr>     */
+/*                                             */
+/*   Created: 2023/03/29 20:39:55 by ramhouch  */
+/*   Updated: 2023/03/29 20:41:16 by ramhouch  */
+/*                                             */
+/* ******************************************* */
+
+# include "main.h"
+
+/**
+ * _strncpy - Function that copies a string.
+ * @dest: Arg 1.
+ * @src: Arg 2.
+ * @n: Arg 3.
+ * Return: A pointer to the resulting string.
+ */
+char	*_strncpy(char *dest, char *src, int n)
+{
+	int	y;
+
+	y = 0;
+	while (src[y] && y < n)
+	{
+		dest[y] = src[y];
+		y++;
+	}
+	dest[y] = '\0';
+	return (dest);
+}
