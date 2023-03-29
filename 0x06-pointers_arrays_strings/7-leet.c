3044f35b9df8f3977d066a15a57f657c6d2fef7c
Rachid Amhouch <ramhouch@e2r9p5.1337.ma>
2023-03-29 22:07:58 +0000

    Push

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -13,35 +13,29 @@
 # include "main.h"
 
 /**
- * check - fcheck.
- * @a: Arg 1.
- * @b: Arg 2.
- * Return: 1 if true.
- */
-int	check(char	a, char	b)
-{
-	if (a == b || a == b - ('A' - 'a'))
-		return (1);
-	return (0);
-}
-
-/**
  * leet - function that encodes a string into 1337.
  * @s: Arg 1.
  * Return: A pointer to the resulting string.
  */
 char	*leet(char *s)
 {
-	int	i;
+	char	*a;
+	char	*b;
+	int		i;
+	int		j;
 
+	a = "aAeEoOtTlL";
+	b = "4433007711";
 	i = 0;
 	while (s[i])
 	{
-		s[i] = check(s[i], 'a') * '4' + !check(s[i], 'a') * s[i];
-		s[i] = check(s[i], 'e') * '3' + !check(s[i], 'e') * s[i];
-		s[i] = check(s[i], 'o') * '0' + !check(s[i], 'o') * s[i];
-		s[i] = check(s[i], 't') * '7' + !check(s[i], 't') * s[i];
-		s[i] = check(s[i], 'l') * '1' + !check(s[i], 'l') * s[i];
+		j = 0;
+		while (a[j])
+		{
+			if (s[i] == a[j])
+				s[i] = b[j];
+			j++;
+		}
 		i++;
 	}
 	return (s);
