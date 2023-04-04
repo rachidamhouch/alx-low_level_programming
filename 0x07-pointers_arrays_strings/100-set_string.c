1eca61c58c9ab4ef530366fd19aa7880f5935fa4
Rachid Amhouch <ramhouch@e2r10p8.1337.ma>
2023-04-04 12:17:26 +0000

    Push

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   100-set_string.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ramhouch <ramhouch@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/04 12:15:37 by ramhouch          #+#    #+#             */
+/*   Updated: 2023/04/04 12:17:04 by ramhouch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "main.h"
+
+/**
+ * set_string - function that sets the value of
+ * a pointer to a char.
+ * @s: Arg 1.
+ * @to: Arg 2.
+ */
+void	set_string(char **s, char *to)
+{
+	*s = to;
+}
