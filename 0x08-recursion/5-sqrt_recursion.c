f9e2f446fc46610ff5ebee128554ed4e49f5169e
Rachid amhouch <rachid.amhouch@gmail.com>
2023-04-05 20:06:27 +0000

    Push

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * sqrtrecursion - function that returns the natural square root of a number.
+ * @n: Arg 1.
+ * @i: Arg 2.
+ * Return: the natural square root of a number.
+ */
+int sqrtrecursion(int n, int i)
+{
+	if (i * i > n)
+        return (-1);
+    if (i * i == n)
+        return (i);
+    return (sqrtrecursion(n, i + 1));
+}
+
+/**
+ * _sqrt_recursion - function that returns the natural square root of a number.
+ * @n: Arg 1.
+ * Return: the natural square root of a number.
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+    sqrtrecursion(n, 1);
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -12,5 +12,6 @@ void	_puts_recursion(char *);
 void	_print_rev_recursion(char *s);
 int 	factorial(int n);
 int		_pow_recursion(int x, int y);
+int		_sqrt_recursion(int n);
 
 #endif
