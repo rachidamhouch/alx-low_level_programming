7bcea56da0f12819dd22404d08cf59aed52f1bc9
Rachid amhouch <rachid.amhouch@gmail.com>
2023-04-05 18:13:55 +0000

    Push

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - function that prints a string in reverse.
+ * @s: Arg 1.
+ */
+ void _print_rev_recursion(char *s)
+{
+	if (*s)
+		_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
