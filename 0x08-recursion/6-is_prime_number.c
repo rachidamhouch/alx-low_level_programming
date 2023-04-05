e170154bd5b1b84a543dba01aaccd2947d2cedeb
Rachid amhouch <rachid.amhouch@gmail.com>
2023-04-05 20:19:33 +0000

    Push

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -4,6 +4,7 @@
  * is_prime_number2 -  function that returns 1 if the
  * input integer is a prime number, otherwise return 0.mber.
  * @n: Arg 1.
+ * @i: Arg 1.
  * Return: 1 or 0;
  */
 int is_prime_number2(int n, int i)
@@ -23,7 +24,7 @@ int is_prime_number2(int n, int i)
  */
 int is_prime_number(int n)
 {
-	if (n < 0)
+	if (n < 0 || n == 1)
 		return (0);
-	return (is_prime_number2(n, 1));
+	return (is_prime_number2(n, 2));
 }
