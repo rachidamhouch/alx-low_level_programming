e924568fcc7dd189a3a48b01490c76f919e9c4bd
Rachid amhouch <rachid.amhouch@gmail.com>
2023-04-07 15:36:22 +0000

    Push

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+/**
+ * _isnum - function that checks if is num.
+ * @c: first arg.
+ * Return: Returns 1 if is num.
+ */
+int _isnum(char	*str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] < '0' || str[i] > '9')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+/**
+ * main - main function.
+ * @ac: arg 1.
+ * @av: arg 1.
+ * Return: 0 if the program finish whitout errors.
+ */
+int	main(int ac, char **av)
+{
+	int	i;
+	int	sum;
+
+	i = 1;
+	sum = 0;
+	while (i < ac)
+	{
+		if (!_isnum(av[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += atoi(av[i]);
+		i++;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
