233c92c18cc3e65771b7b2aea9594a5267c56bba
Rachid amhouch <rachid.amhouch@gmail.com>
2023-04-07 15:50:52 +0000

    Push

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,6 +2,38 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+/**
+ * _atoi - function that convert a string to an integer.
+ * @s: arg 1.
+ * Return: an integer.
+ */
+int _atoi(char *s)
+{
+	unsigned int	n;
+	int				sign;
+	int				i;
+
+	n = 0;
+	i = 0;
+	sign = 0;
+	while (s[i] && (s[i] == '+' || s[i] == '-' || s[i] == ' ' || s[i] == '\t'))
+	{
+		if (s[i] == '-')
+			sign++;
+		i++;
+	}
+	while (s[i] && (s[i] < '0' || s[i] > '9'))
+		i++;
+	while (s[i] && s[i] >= '0' && s[i] <= '9')
+	{
+		n *= 10;
+		n += s[i] - '0';
+		i++;
+	}
+	if (sign % 2 == 0)
+		return (n);
+	return (-n);
+}
 
 /**
  * _isnum - function that checks if is num.
@@ -42,8 +74,8 @@ int	main(int ac, char **av)
 			printf("Error\n");
 			return (1);
 		}
-		if (atoi(av[i]) > 0)
-			sum += atoi(av[i]);
+		if (_atoi(av[i]) > 0)
+			sum += _atoi(av[i]);
 		i++;
 	}
 	printf("%d\n", sum);
