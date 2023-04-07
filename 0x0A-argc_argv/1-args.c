19722ca2efc8987eb6c2d6b24283c274c0d458b3
Rachid amhouch <rachid.amhouch@gmail.com>
2023-04-07 15:09:49 +0000

    Push

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * main - main function.
+ * @ac: arg 1.
+ * @av: arg 1.
+ * Return: 0 if the program finish whitout errors.
+ */
+int	main(int ac, char **av)
+{
+	(void)av;
+	printf("%d\n", ac);
+	return (0);
+}
