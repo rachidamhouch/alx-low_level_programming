3c8e04bb49e33744079302ecf097909219743179
Rachid amhouch <rachid.amhouch@gmail.com>
2023-04-11 11:48:46 +0000

    Push

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,6 +1,33 @@
 #include "main.h"
 
 /**
+ * _strdup - function that returns a pointer
+ * to a newly allocated space in memory, which
+ * contains a copy of the string given as a parameter.
+ * @str: Arg 1.
+ * Return: Returns a pointer to the array.
+ */
+char	*_strdup(char *str)
+{
+	char	*str1;
+	size_t	i;
+
+	if (!str)
+		return (0);
+	str1 = malloc(strlen(str) + 1);
+	i = 0;
+	if (!str1)
+		return (0);
+	while (i < strlen(str))
+	{
+		str1[i] = str[i];
+		i++;
+	}
+	str1[strlen(str)] = '\0';
+	return (str1);
+}
+
+/**
  * str_concat - function that concatenates two strings.
  * @s1: Arg 1.
  * @s2: Arg 1.
@@ -13,7 +40,7 @@ char	*str_concat(char *s1, char *s2)
 	size_t	t;
 
 	if (!s1)
-		return (strdup(s2));
+		return (_strdup(s2));
 	str = malloc(strlen(s1) + strlen(s2) + 1);
 	i = 0;
 	t = 0;
