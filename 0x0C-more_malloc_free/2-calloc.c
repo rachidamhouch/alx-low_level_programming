18fb36a8d323e9a52b7f97f367122ca346053954
Rachid amhouch <rachid.amhouch@gmail.com>
2023-04-13 16:19:15 +0000

    Push

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,6 +1,27 @@
 #include "main.h"
 
 /**
+ * _memset - function that fills memory with a constant byte.
+ * @s: Arg 1.
+ * @b: Arg 2.
+ * @n: Arg 2.
+ * Return: A pointer to the resulting string.
+ */
+void	*_memset(char *s, char b, unsigned int n)
+{
+	int	i;
+
+	i = 0;
+	while (n)
+	{
+		s[i] = b;
+		i++;
+		n--;
+	}
+	return (s);
+}
+
+/**
  * _calloc - function that allocates memory for an array.
  * @nmemb: Arg 1.
  * @size: arg 2.
@@ -14,6 +35,6 @@ void	*_calloc(unsigned int nmemb, unsigned int size)
 		return (0);
 	ptr = malloc(nmemb * size);
 	if (ptr)
-		memset(ptr, 0, nmemb * size);
+		_memset(ptr, 0, nmemb * size);
 	return (ptr);
 }
