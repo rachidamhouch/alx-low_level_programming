042cf31d3f9b593d68243d7e52e8c3dc6da8258c
Rachid amhouch <rachid.amhouch@gmail.com>
2023-04-13 15:59:30 +0000

    Push

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,10 +15,13 @@ char	*string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int		i;
 	unsigned int		j;
 
-	if (!s1 || !s2)
-		return (NULL);
-	len1 = strlen(s1);
-	len2 = strlen(s2);
+
+	len1 = 0;
+	len2 = 0;
+	if (s1)
+		len1 = strlen(s1);
+	if (s2)
+		len2 = strlen(s2);
 	i = 0;
 	j = 0;
 	if (n < len2)
