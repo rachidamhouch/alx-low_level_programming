3a334b917afe691df2b174d845f6c087b14d936e
Rachid amhouch <rachid.amhouch@gmail.com>
2023-04-17 16:58:42 +0000

    anything

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+
+/**
+ * free_dog - function that frees dogs.
+ * @d: Arg 1.
+ */
+void free_dog(dog_t *d)
+{
+    if (!d)
+        return;
+    free(d->name);
+    free(d->owner);
+    free(d);
+}
