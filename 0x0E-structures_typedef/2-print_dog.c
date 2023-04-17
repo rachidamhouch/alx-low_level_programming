7910953d6c588665a4b101cbd0c701efe99a87d7
Rachid amhouch <rachid.amhouch@gmail.com>
2023-04-17 16:23:41 +0000

    anything

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,6 +9,6 @@ void print_dog(struct dog *d)
 	if (!d)
 		return;
 	printf("Name: %s\n", d->name);
-	printf("Age: %s\n", d->age);
+	printf("Age: %f\n", d->age);
 	printf("Owner: %s\n", d->owner);
 }
