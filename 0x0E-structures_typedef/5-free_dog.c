eb1af7070928d64dd073ee2a9cf8dd0b2963321a
Rachid amhouch <rachid.amhouch@gmail.com>
2023-04-17 17:00:03 +0000

    anything

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -6,9 +6,9 @@
  */
 void free_dog(dog_t *d)
 {
-    if (!d)
-        return;
-    free(d->name);
-    free(d->owner);
-    free(d);
+	if (!d)
+		return;
+	free(d->name);
+	free(d->owner);
+	free(d);
 }
