30f91965cd82e4507848003c0249bc3be34814ad
Rachid amhouch <rachid.amhouch@gmail.com>
2023-04-17 16:16:29 +0000

    anything

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,6 @@
 #ifndef DOG_H
 # define DOG_H
 #include <stdio.h>
-void init_dog(struct dog *d, char *name, float age, char *owner);
 
 /**
  * struct dog - Dog info
@@ -15,4 +14,6 @@ struct dog
 	float	age;
 	char	*owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
