e06e2eb56f6407360001af33a934b7f529eaf8a7
Rachid amhouch <rachid.amhouch@gmail.com>
2023-04-19 16:43:56 +0000

    anything

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -7,6 +7,18 @@
 # include <time.h>
 # include <string.h>
 
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+typedef struct op
+{
+    char *op;
+    int (*f)(int a, int b);
+} op_t;
+
 int		_putchar(char c);
 void    print_name(char *name, void (*f)(char *));
 void    array_iterator(int *array, size_t size, void (*action)(int));
