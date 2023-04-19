a936b530be4f2093d9d872167852c6e98c2eb789
Rachid amhouch <rachid.amhouch@gmail.com>
2023-04-19 15:43:30 +0000

    anything

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - function that prints a name.
+ * @array: arg 1.
+ * @size: arg 2.
+ * @action: arg 3.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t	i;
+
+	if (!action)
+		return;
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
