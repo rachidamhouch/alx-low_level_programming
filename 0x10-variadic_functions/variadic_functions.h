38fac591544c3dca83c96a278eb73f2c632e7bb6
Rachid amhouch <rachid.amhouch@gmail.com>
2023-04-20 16:56:57 +0000

    anything

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,10 +1,7 @@
-#ifndef VARIADIC_FUNCTIONS_H
-# define VARIADIC_FUNCTIONS_H
-# include <stdio.h>
-# include <stdlib.h>
-# include <unistd.h>
-# include <string.h>
-# include <stdarg.h>
+#ifndef VAR_FUNC_H
+#define VAR_FUNC_H
+#include <stdio.h>
+#include <stdarg.h>
 
 int sum_them_all(const unsigned int n, ...);
 #endif
