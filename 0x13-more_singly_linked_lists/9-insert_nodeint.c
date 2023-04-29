4c9207da4d392a3a950b65b56bd676e524a35f02
Rachid amhouch <rachid.amhouch@gmail.com>
2023-04-29 14:07:02 +0100

    anything

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,7 +13,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int	i = 0;
 	listint_t *tmp, *node, *old;
 
-	if (!head || !*head)
+	if (!head)
 		return (NULL);
 	tmp = *head;
 	while (tmp)
@@ -42,7 +42,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			return (NULL);
 		node->n = n;
 		node->next = NULL;
-		old->next = node;
+		if (!i)
+			*head = node;
+		else
+			old->next = node;
 		return (node);
 	}
 	return (NULL);
