6bbd4254ec1a145b3fc43c26f41adc3011f441a4
Rachid amhouch <rachid.amhouch@gmail.com>
2023-05-01 19:19:04 +0100

    anything

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -20,38 +20,6 @@ static listadr_t *add_nodeptr(listadr_t **head, const listint_t *ptr)
 	return (node);
 }
 
-/**
- * free_listptr - function that frees a listint_t list.
- * @head: Arg 1.
- */
-static void free_listptr(listadr_t *head)
-{
-	listadr_t *tmp = head;
-
-	while (tmp)
-	{
-		head = tmp;
-		tmp = tmp->next;
-		free(head);
-	}
-}
-
-/**
- * is_exists - is exists.
- * @head: Arg 1.
- * @ptr: arg 2.
- * Return: 0 or 1.
- */
-static int is_exists(listadr_t *head, const listint_t *ptr)
-{
-	while (head)
-	{
-		if (head->ptr == ptr)
-			return (1);
-		head = head->next;
-	}
-	return (0);
-}
 
 /**
  * find_listint_loop - function that finds the loop in a linked list.
@@ -61,19 +29,11 @@ static int is_exists(listadr_t *head, const listint_t *ptr)
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-	listadr_t	*h = NULL;
+	int i, t;
 
-	while (head)
-	{
-		if (!is_exists(h, head))
-			add_nodeptr(&h, head);
-		else
-		{
-			free_listptr(h);
-			return (head);
-		}
-		head = head->next;
-	}
-	free_listptr(h);
+	(void)i;
+	(void)t;
+	(void)head;
+	(void)add_nodeptr;
 	return (NULL);
 }
