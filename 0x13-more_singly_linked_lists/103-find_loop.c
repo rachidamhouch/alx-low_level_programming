ae6387bcd8d9af84fb2f903014bcee8641d3750c
Rachid amhouch <rachid.amhouch@gmail.com>
2023-05-01 19:36:39 +0100

    anything

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -11,9 +11,6 @@ listint_t *find_listint_loop(listint_t *head)
 	listint_t *slow = head;
 	listint_t *fast = head;
 
-	if (!head)
-		return (NULL);
-
 	while (slow && slow->next && fast && fast->next)
 	{
 		fast = fast->next->next;
