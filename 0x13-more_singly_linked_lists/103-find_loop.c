7bdb160fd886f1dae18b5eb3cc70935bc6fc8c96
Rachid amhouch <rachid.amhouch@gmail.com>
2023-05-01 19:19:54 +0100

    anything

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,27 +1,6 @@
 #include "lists.h"
 
 /**
- * add_nodeptr - function that adds a new node
- * at the beginning of a listadr_t list.
- * @head: Arg 1.
- * @ptr: arg 2.
- * Return: the address of the new element, or NULL if it failed.
- */
-static listadr_t *add_nodeptr(listadr_t **head, const listint_t *ptr)
-{
-	listadr_t	*node;
-
-	node = malloc(sizeof(listadr_t));
-	if (!node || !head)
-		return (NULL);
-	node->ptr = ptr;
-	node->next = *head;
-	*head = node;
-	return (node);
-}
-
-
-/**
  * find_listint_loop - function that finds the loop in a linked list.
  * @head: Arg 1.
  * Return: The address of the node where the
@@ -33,7 +12,6 @@ listint_t *find_listint_loop(listint_t *head)
 
 	(void)i;
 	(void)t;
-	(void)head;
-	(void)add_nodeptr;
+	(void)head;;
 	return (NULL);
 }
