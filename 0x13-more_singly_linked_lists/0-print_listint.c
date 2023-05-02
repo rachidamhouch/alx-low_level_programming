173b662b5662380ff2dbefbb85e5fb4debc00b10
Rachid amhouch <rachid.amhouch@gmail.com>
2023-05-02 11:49:49 +0100

    anything

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * print_listint - function that prints all the elements of a listint_t list.
+ * print_listin - function that prints all the elements of a listint_t list.
  * @h: Arg 1.
  * Return: the number of nodes.
  */
