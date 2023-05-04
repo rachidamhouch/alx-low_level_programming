277b113738b00b3e70abe128839618ce28e10890
Rachid amhouch <rachid.amhouch@gmail.com>
2023-05-04 14:45:18 +0100

    anything

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+ * print_binary - function that prints the binary representation of a number.
+ * @n: Arg 1.
+ */
+void print_binary(unsigned long int n)
+{
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -7,4 +7,5 @@
 # include <string.h>
 
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
 #endif
