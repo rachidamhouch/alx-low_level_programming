c8b10951326bc951201f9b0daa2aa4025ad6e4df
Rachid amhouch <rachid.amhouch@gmail.com>
2023-05-04 15:33:18 +0100

    anything

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,4 +1,17 @@
 #include "main.h"
+/**
+ * help_print_binary - function that prints
+ * the binary representation of a number.
+ * @n: Arg 1.
+ */
+void help_print_binary(unsigned long int n)
+{
+	if (n)
+		help_print_binary(n >> 1);
+	else
+		return;
+	printf("%lu", n & 1);
+}
 
 /**
  * print_binary - function that prints the binary representation of a number.
@@ -6,4 +19,7 @@
  */
 void print_binary(unsigned long int n)
 {
+	if (!n)
+		printf("0");
+	help_print_binary(n);
 }
