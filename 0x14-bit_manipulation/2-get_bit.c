0ac77205afd64e8f20044b2352bce63dc8c6fc94
Rachid amhouch <rachid.amhouch@gmail.com>
2023-05-04 16:11:43 +0100

    anything

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,7 +8,7 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index >= sizeof(unsigned long int))
+	if (index > sizeof(unsigned long int))
 		return (-1);
 	return ((n >> index) & 1);
 }
