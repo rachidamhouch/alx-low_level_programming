c8acd580c6cec067a5ab132ec681b3c1276850da
Rachid amhouch <rachid.amhouch@gmail.com>
2023-05-04 11:10:24 +0100

    anything

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,4 +7,22 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
+	unsigned int	n = 0, t = 1;
+	int				i = 0;
+
+	if (!b)
+		return (0);
+	i = strlen(b) - 1;
+	while (i >= 0)
+	{
+		if (b[i] == '0' || b[i] == '1')
+		{
+			n += (b[i] - '0') * t;
+			t *= 2;
+			i--;
+		}
+		else
+			return (0);
+	}
+	return (n);
 }
