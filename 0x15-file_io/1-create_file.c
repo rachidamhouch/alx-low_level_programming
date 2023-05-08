4e23c3827a1d79add8eff5eadaa190daab32b856
Rachid amhouch <rachid.amhouch@gmail.com>
2023-05-08 16:49:41 +0100

    anything

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,6 +18,10 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 	if (write(fd, text_content, strlen(text_content)) == -1)
+	{
+		close(-1);
 		return (-1);
+	}
+	close(-1);
 	return (0);
 }
