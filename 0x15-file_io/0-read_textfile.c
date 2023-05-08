d02c7652f1c14a546acd4803b8f62ea513d23f56
Rachid amhouch <rachid.amhouch@gmail.com>
2023-05-08 16:18:37 +0100

    anything

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,7 +16,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (!filename)
 		return (0);
 	c = malloc(letters);
-	fd = open(filename, O_CREAT | O_RDONLY);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1 || !c)
 	{
 		free(c);
