3991007c80ae2265e1a6bdb75523f6ef1dd8e728
Rachid amhouch <rachid.amhouch@gmail.com>
2023-05-08 16:28:32 +0100

    anything

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,19 +9,19 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t o, r, w;
+ssize_t o, r, w;
 	char *buffer;
 
-	if (!filename)
+	if (filename == NULL)
 		return (0);
 
-	buffer = malloc(letters);
-	if (!buffer)
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
 		return (0);
 
 	o = open(filename, O_RDONLY);
 	r = read(o, buffer, letters);
-	w = write(1, buffer, r);
+	w = write(STDOUT_FILENO, buffer, r);
 
 	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
