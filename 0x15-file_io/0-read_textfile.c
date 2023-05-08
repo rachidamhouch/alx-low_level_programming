e2d06115253cd6899e9443668da1a4d3968650a5
Rachid amhouch <rachid.amhouch@gmail.com>
2023-05-08 16:27:34 +0100

    anything

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -22,9 +22,15 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	o = open(filename, O_RDONLY);
 	r = read(o, buffer, letters);
 	w = write(1, buffer, r);
+
+	if (o == -1 || r == -1 || w == -1 || w != r)
+	{
+		free(buffer);
+		return (0);
+	}
+
 	free(buffer);
 	close(o);
-	if (o == -1 || r == -1 || w == -1 || w != r)
-		return (0);
+
 	return (w);
 }
