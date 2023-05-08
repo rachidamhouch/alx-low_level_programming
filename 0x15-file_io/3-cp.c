c5dc331f91b30889a798a6b1a657616d73d49cce
Rachid amhouch <rachid.amhouch@gmail.com>
2023-05-08 19:18:41 +0100

    anything

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -32,5 +32,17 @@ int main(int ac, char **av)
 		}
 		fd_out = open(av[2], O_WRONLY | O_APPEND);
 	}
+	if (close(fd_in) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_in);
+		if (close(fd_out) == -1)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_out);
+		return (100);
+	}
+	if (close(fd_out) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_out);
+		return (100);
+	}
 	return (0);
 }
