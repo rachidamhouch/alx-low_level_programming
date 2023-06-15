1cf3cafda6544c9a63c804bd05681455e3575e15
rachidamhouch <rachid.amhouch@gmail.com>
2023-06-15 11:11:45 +0100

    anything

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,5 +1,10 @@
 #include "lists.h"
 
+/**
+ * print_dlistint - function that prints list.
+ * @h: Arg 1.
+ * Return: The number of elm.
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
 	int i = 0;
