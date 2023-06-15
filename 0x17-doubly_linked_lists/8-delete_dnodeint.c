6d90834c1d39eb0b468757c856f0b601145d4d26
rachidamhouch <rachid.amhouch@gmail.com>
2023-06-15 17:10:45 +0100

    anything

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,56 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - function that returns the nth
+ * node of a dlistint_t linked list.
+ * @head: Arg 1.
+ * @index: arg 2.
+ * Return: the address of an element, or NULL
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+
+	while (head)
+	{
+		if (i == index)
+			return (head);
+		head = head->next;
+		i++;
+	}
+	return (NULL);
+}
+
+/**
+ * delete_dnodeint_at_index - function that deletes the
+ * node at index index of a dlistint_t linked list.
+ * @head: Arg 1.
+ * @index: arg 2.
+ * Return: 1 or -1.
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *node = get_dnodeint_at_index(*head, index);
+
+	if (!node)
+		return (-1);
+	if (!node->prev)
+	{
+		if (node->next)
+		{
+			node->next->prev = NULL;
+			*head = node->next;
+		}
+		else
+			*head = NULL;
+	}
+	else if (!node->next)
+		node->prev->next = NULL;
+	else
+	{
+		node->prev->next = node->next;
+		node->next->prev = node->prev;
+	}
+	free(node);
+	return (0);
+}
