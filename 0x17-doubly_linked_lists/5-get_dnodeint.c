ccfee15342f17649fdfb3013e7e7adeb83caab30
rachidamhouch <rachid.amhouch@gmail.com>
2023-06-15 13:27:01 +0100

    anything

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -9,13 +9,13 @@
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-    unsigned int i = 0;
-    while (head)
-    {
-        if (i == index)
-            return (head);
-        head = head->next;
-        i++;
-    }
-    return (NULL);
+	unsigned int i = 0;
+	while (head)
+	{
+		if (i == index)
+			return (head);
+		head = head->next;
+		i++;
+	}
+	return (NULL);
 }
