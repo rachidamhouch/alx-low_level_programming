916874534c93e4c98e40d0732e12c6cec9290236
rachidamhouch <rachid.amhouch@gmail.com>
2023-06-15 11:22:58 +0100

    anything

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -15,10 +15,9 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (!node || !head)
 		return (NULL);
 	node->n = n;
-	node->next = NULL;
+	node->next = *head;
 	node->prev = NULL;
-	if (*head)
-		node->next = *head;
+	(*head)->next->prev = node;
 	*head = node;
 	return (node);
 }
