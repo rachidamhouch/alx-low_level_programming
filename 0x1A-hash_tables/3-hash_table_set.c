4d0b03f74c31152a271dc3194cc6ebf609dc2e5f
Rachid amhouch <rachid.amhouch@gmail.com>
2023-07-07 03:10:00 +0100

    anything

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,14 +1,13 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_set - ...
- * @ht: ...
+ * make_node - ...
  * @key: ...
  * @value: ...
  *
- * Return: 1 0r 0
+ * Return: ...
  */
-int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+hash_node_t *make_node(const char *key, const char *value)
 {
 	hash_node_t *node;
 
@@ -30,4 +29,44 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 	node->next = 0;
 	return (node);
+}
+
+/**
+ * hash_table_set - ...
+ * @ht: ...
+ * @key: ...
+ * @value: ...
+ *
+ * Return: 1 0r 0
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *hash_node, *tmp;
+	char *new_value;
+
+	if (!ht || !ht->array || !ht->size ||
+	    !key || !strlen(key) || !value)
+		return (0);
+	index = key_index((const unsigned char *)key, ht->size);
+	tmp = ht->array[index];
+	while (tmp)
+	{
+		if (!strcmp(tmp->key, key))
+		{
+			new_value = strdup(value);
+			if (!new_value)
+				return (0);
+			free(tmp->value);
+			tmp->value = new_value;
+			return (1);
+		}
+		tmp = tmp->next;
+	}
+	hash_node = make_node(key, value);
+	if (!hash_node)
+		return (0);
+	hash_node->next = ht->array[index];
+	ht->array[index] = hash_node;
+	return (1);
 }
